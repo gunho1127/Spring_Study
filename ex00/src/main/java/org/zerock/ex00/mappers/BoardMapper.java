16a71a1564f5866ae2f1ed8874c6a124089bda06
gunho11127 <rjsghsky@gmail.com>
2024-10-30 17:36:56 +0900

    no message

--- ex00/src/main/java/org/zerock/ex00/mappers/BoardMapper.java
+++ ex00/src/main/java/org/zerock/ex00/mappers/BoardMapper.java
@@ -11,6 +11,8 @@ public interface BoardMapper {
 
     List<BoardVO> getPage(Criteria criteria);
 
+    int getTotal(Criteria criteria);
+
     int insert(BoardVO boardVO);
 
     BoardVO select(Long bno);
